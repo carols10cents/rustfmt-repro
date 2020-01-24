9092938f20440664afe0697820ad6f0ee75b13f5
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2020-01-24 09:49:38 -0500

    Add code to reproduce the issue

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+fn myfunc1() {
+    println!("hi");      // yes I want my comments here
+}       // keep this comment here too
+
+fn myfunc2() {
+    println!("bye");        // i want it that way
+}    // tell me why
