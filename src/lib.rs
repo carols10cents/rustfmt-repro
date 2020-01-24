9c3f0f0a08395d702f2f8e19cfa6131e63b09d7e
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2020-01-24 09:50:27 -0500

    what happens with stock rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 fn myfunc1() {
-    println!("hi");      // yes I want my comments here
-}       // keep this comment here too
+    println!("hi"); // yes I want my comments here
+} // keep this comment here too
 
 fn myfunc2() {
-    println!("bye");        // i want it that way
-}    // tell me why
+    println!("bye"); // i want it that way
+} // tell me why
